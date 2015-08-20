85d68a5f82063cd98e7bace9ec81a8dc1b19c84d
5730337521-PC <singto_victor@hotmail.com>
2015-08-21 06:47:04 +0700

    Implement greetingMassage

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -15,7 +15,7 @@ public class Hello {
 
 	public static String greetingMessage(){
 		// TODO Implement this method to return string "Hello GitHub".
-		return "";
+		return "Greeting Massage";
 	}
 	
 	public static int[] bubbleSort(int[] array){
